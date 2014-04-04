46db33184bc5e740c8fa4822c7cbd54a8f5064a5
Simon Kirkby <tigger@interthingy.com>
2014-04-04 13:15:36 +0800

    multi os work.
    Signed-off-by: Simon Kirkby <tigger@interthingy.com>

--- control.py
+++ control.py
@@ -1,8 +1,54 @@
-#!/usr/bin/python
+#!/usr/bin/python -i
 
 # -*- coding: utf-8 -*-
 
 # some basic tools for bootserver manipulation
 
+import os 
 
+import config
+from model import * 
 
+import readline,rlcompleter
+readline.parse_and_bind('tab:complete')
+
+# load configs
+pxe_menu = yaml.load(open(config.menu))
+distros = yaml.load(open('config/image_urls.txt'))
+
+def get_media():
+    if 'media' in dir(config):
+        path = config.media
+    else:
+        path = 'static/images'
+    for i in distros.keys():        
+        if distros[i] != None:
+            print '\n'+i
+            # check if folder exists
+            try:
+                os.stat(path+os.sep+i)
+            except:
+                print 'no folder '+i
+                os.mkdir(path+os.sep+i)
+            # check if processro folder is there
+            for j in distros[i]:
+                print '\t '+j
+                directory = path+os.sep+i+os.sep+j
+                try:
+                    os.stat(directory)
+                except:
+                    os.mkdir(directory)
+                data = distros[i][j]
+                kernel = directory+os.sep+data['boot']
+                file_system =  directory+os.sep+data['fs']
+                try:
+                    os.stat(kernel)
+                except:
+                    print 'get file ' + str(data['kernel'])
+                try:
+                    os.stat(file_system)
+                except:
+                    print 'get file ' + str(data['initrd'])    
+
+if __name__ == "__main__":
+    get_media()
